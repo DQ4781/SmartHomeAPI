afb9090daa484e443be8e13cdfe531584e1e745a
Daniel Quezada <dquezada478@gmail.com>
2024-03-12 15:28:23 -0700

    update current_user_id in sql statement to tuple

--- thermostat.py
+++ thermostat.py
@@ -8,7 +8,9 @@ thermostat = Blueprint("thermostat", __name__, url_prefix="/thermostat")
 def index():
     # Fetch connected thermostat devices from the database
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM thermostat_devices WHERE user_id = %s", current_user_id)
+    cur.execute(
+        "SELECT * FROM thermostat_devices WHERE user_id = %s", (current_user_id,)
+    )
     devices = cur.fetchall()
     return render_template("thermostat.html", devices=devices)
 
@@ -44,6 +46,6 @@ def delete_device():
     device_id = request.form.get("device_id")
     # Delete the device from the database
     cur = mysql.connection.cursor()
-    cur.execute("DELETE FROM thermostat_devices WHERE id = %s", device_id)
+    cur.execute("DELETE FROM thermostat_devices WHERE id = %s", (device_id,))
     mysql.connection.commit()
     return redirect(url_for("thermostat.index"))
