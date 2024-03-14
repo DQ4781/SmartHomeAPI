4d6127ad4208e0a8bd0349dfe6168dd63eb30cf5
Daniel Quezada <dquezada478@gmail.com>
2024-03-14 11:09:24 -0700

    updated imports and how blueprints go to routes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, url_for
-from flask_mysqldb import MySQL
+from db import mysql
 
 
 app = Flask(__name__)
@@ -8,33 +8,27 @@ app = Flask(__name__)
 # Secret Key for Session Managment
 app.secret_key = "thisisasecretkey"
 
-# DB CONNECTION STUFF: TODO AT A LATER TIME
-
+# MySQL configs
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = "pass"
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_DB"] = "smart_home"
 
-mysql = MySQL(app)
+mysql.init_app(app)
 
 
-import auth
-import home
-import light
-import thermostat
-import profiles
+from auth import auth
+from home import home
+from light import light
+from thermostat import thermostat
+from profiles import profile
 
 
-app.register_blueprint(auth.auth)
-app.register_blueprint(home.home)
-app.register_blueprint(light.light)
-app.register_blueprint(thermostat.thermostat)
-app.register_blueprint(profiles.profile)
-
-
-@app.route("/")
-def index():
-    return redirect(url_for("auth.register"))
+app.register_blueprint(auth)
+app.register_blueprint(home)
+app.register_blueprint(light)
+app.register_blueprint(thermostat)
+app.register_blueprint(profile)
 
 
 if __name__ == "__main__":
