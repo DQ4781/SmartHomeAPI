c12100307cd916892bb874fb59506700600fbc85
Daniel Quezada <dquezada478@gmail.com>
2024-03-14 10:58:38 -0700

    updated index endpoint for profile.py

--- profiles.py
+++ profiles.py
@@ -1,8 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, session, flash
+from app import mysql
 
 profile = Blueprint("profile", __name__, url_prefix="/profile")
 
 
 @profile.route("/")
 def index():
-    return render_template("profile.html")
+    current_user_id = session.get("user_id")
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM users WHERE id = %s", (current_user_id,))
+    user_details = cur.fetchone()
+    return render_template("profile.html", user=user_details)
