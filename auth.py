ec6c2645c90f925726981acdb14897e26720d2c8
Daniel Quezada <dquezada478@gmail.com>
2024-03-14 10:47:42 -0700

    updated auth.py login functionality

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, session
 from app import mysql
 
 auth = Blueprint("auth", __name__, url_prefix="/auth")
@@ -6,7 +6,21 @@ auth = Blueprint("auth", __name__, url_prefix="/auth")
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
-    # Login logic here
+    if request.method == "POST":
+        username = request.form["username]"]
+        password = request.form["password"]
+        cur = mysql.connection.cursor()
+        cur.execute(
+            "SELECT * FROM users WHERE username = %s AND password = %s",
+            (username, password),
+        )
+        account = cur.fetchone()
+        if account:
+            session["user_id"] = account["id"]  # Store user ID in session
+            return redirect(url_for("home.index"))
+        else:
+            msg = "Incorrect username or password!"
+            return render_template("login.html", msg=msg)
     return render_template("login.html")
 
 
