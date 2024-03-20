642a41d6ff057c98565bc51d20bdc89d1dbb67f6
Daniel Quezada <dquezada478@gmail.com>
2024-03-19 21:31:38 -0700

    added json functionality

--- thermostat.py
+++ thermostat.py
@@ -1,4 +1,12 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    session,
+    jsonify,
+)
 from db import mysql
 
 thermostat = Blueprint("thermostat", __name__, url_prefix="/thermostat")
@@ -18,7 +26,11 @@ def index():
 
 @thermostat.route("/add", methods=["POST"])
 def add_device():
-    room = request.form.get("room")
+    if request.is_json:
+        data = request.json
+        room = data.get("room")
+    else:
+        room = request.form.get("room")
     current_user_id = session.get("user_id")
     # Add the new device to the database
     cur = mysql.connection.cursor()
@@ -27,27 +39,36 @@ def add_device():
         (current_user_id, room, 50),
     )
     mysql.connection.commit()
-    return redirect(url_for("thermostat.index"))
+    return jsonify({"message": "Thermostat Device added successfully"}), 200
 
 
 @thermostat.route("/update", methods=["POST"])
 def update_device():
-    device_id = request.form.get("device_id")
-    setting = request.form.get("setting")
+    if request.is_json:
+        data = request.json
+        device_id = data.get("device_id")
+        setting = data.get("setting")
+    else:
+        device_id = request.form.get("device_id")
+        setting = request.form.get("setting")
     # Update the device setting in the database
     cur = mysql.connection.cursor()
     cur.execute(
         "UPDATE thermostat_devices SET setting = %s WHERE id = %s", (setting, device_id)
     )
     mysql.connection.commit()
-    return redirect(url_for("thermostat.index"))
+    return jsonify({"message": "Thermostat Device updated successfully"}), 200
 
 
 @thermostat.route("/delete", methods=["POST"])
 def delete_device():
-    device_id = request.form.get("device_id")
+    if request.is_json:
+        data = request.json
+        device_id = data.get("device_id")
+    else:
+        device_id = request.form.get("device_id")
     # Delete the device from the database
     cur = mysql.connection.cursor()
     cur.execute("DELETE FROM thermostat_devices WHERE id = %s", (device_id,))
     mysql.connection.commit()
-    return redirect(url_for("thermostat.index"))
+    return jsonify({"message": "Thermostat Device deleted successfully"}), 200
